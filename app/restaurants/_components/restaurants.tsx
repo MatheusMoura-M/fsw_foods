bd3c2f67e3d6e4e34522d1c965e9e7bb116e7627
Matheus <matheus150101miranda@gmail.com>
2024-06-01 11:01:09 -0300

    feat: ensure restaurants can be favorited through restaurant page

--- app/restaurants/_components/restaurants.tsx
+++ app/restaurants/_components/restaurants.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import Header from "@/app/_components/header";
-import RestaurantItem from "@/app/_components/restaurant-item";
-import { Restaurant } from "@prisma/client";
+import { Restaurant, UserFavoriteRestaurant } from "@prisma/client";
 import { notFound, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { searchForRestaurants } from "../_actions/search";
+import Header from "@/app/_components/header";
+import RestaurantItem from "@/app/_components/restaurant-item";
 
-const Restaurants = () => {
+interface RestaurantProps {
+  userFavoriteRestaurants: UserFavoriteRestaurant[];
+}
+
+const Restaurants = ({ userFavoriteRestaurants }: RestaurantProps) => {
   const searchParams = useSearchParams();
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
 
@@ -16,7 +20,6 @@ const Restaurants = () => {
   useEffect(() => {
     const fetchRestaurants = async () => {
       if (!searchFor) return;
-
       const foundRestaurants = await searchForRestaurants(searchFor);
       setRestaurants(foundRestaurants);
     };
@@ -39,6 +42,7 @@ const Restaurants = () => {
               key={restaurant.id}
               restaurant={restaurant}
               className="min-w-full max-w-full"
+              userFavoriteRestaurants={userFavoriteRestaurants}
             />
           ))}
         </div>
