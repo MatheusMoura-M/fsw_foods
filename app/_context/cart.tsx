e854bfbafe953bc64f2e3b920e3f1045ba946a09
Matheus <matheus150101miranda@gmail.com>
2024-06-01 09:31:40 -0300

    feat: move restaurant list to a client component

--- app/_context/cart.tsx
+++ app/_context/cart.tsx
@@ -141,7 +141,9 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
       include: {
         restaurant: {
           select: {
+            id: true;
             deliveryFee: true;
+            deliveryTimeMinutes: true;
           };
         };
       };
